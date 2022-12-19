87bbd23d556cc4a3eaa122e002a4b52164f5b201
Sumeet3005 <115893571+Sumeet3005@users.noreply.github.com>
2022-12-19 23:48:15 +0530

    ok

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Sumeet_Library_1',
+      version='0.1',
+      description= 'First One',
+      author='Sumeet Mitra',
+      packages=['Sumeet_Library_1'],
+      zip_safe=False)
